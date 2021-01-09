23216fa8316e80b7a2022945eebc73e140309088
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-01-09 00:14:49 -0500

    more updates to socials

--- src/components/SocialsBar.js
+++ src/components/SocialsBar.js
@@ -5,28 +5,28 @@ import './Socials.css';
 function SocialsBar(props){
     
     const flow = props.flow;
-    var pHeight, pWidth;
-    const flowCheck = () =>{
-        var len = props.socials.length
-        if(props.flow === "column"){
-            pHeight = (8 * len) + "vh";
-            pWidth = "auto";
-        }
-        else if(props.flow === "row-reverse"){
-            pHeight = "auto";
-            pWidth = (4 * len) + "vw";
-        }
+    // var pHeight, pWidth;
+    // const flowCheck = () =>{
+    //     var len = props.socials.length
+    //     if(props.flow === "column"){
+    //         pHeight = (8 * len) + "vh";
+    //         pWidth = "auto";
+    //     }
+    //     else if(props.flow === "row-reverse"){
+    //         pHeight = "auto";
+    //         pWidth = (4 * len) + "vw";
+    //     }
 
 
-        return {
-            flexDirection: flow,
-            width: pWidth,
-            height: pHeight
-        };
-    }
+    //     return {
+    //         flexDirection: flow,
+    //         width: pWidth,
+    //         height: pHeight
+    //     };
+    // }
 
     return(
-        <div className="socialsContainer" style={flowCheck()}>
+        <div className="socialsContainer" style={{flexDirection: flow}}>
             {props.socials.map((social) => (
             <a href={social.link} target="_blank">
                 <div className={social.type + "Container"}>
