d4352546410456b7b4df1de8e810ec3fed327001
Tiffany <tiffany1259@gmail.com>
2021-02-07 23:30:42 -0500

    fix about section

--- src/components/Discord.js
+++ src/components/Discord.js
@@ -23,20 +23,22 @@ function Discord() {
             </HeroSection>
             
             <div className="aboutSection">
-                <iframe title="Rutgers Esports" align="right" src="https://discord.com/widget?id=143013824679641088&theme=dark" width="350" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                <h1 class="new-section">About</h1>
-                <h4 class="important">Rutgers Esports Discord is <em>the</em> main hub for all things Rutgers Esports!</h4>
-                <p> {/* You can change this to whatever idrc */}
-                    <ul>
-                        <li>Get important announcements regarding events and opportunities within Rutgers Esports and the larger collegiate Esports scene in the northeast!</li>
-                        <li>Talk to the Rutgers Esports community about trending new titles, upcoming events, or even anime!</li>
-                        <li>Acts as an entry point to numerous clubs under the Rutgers Esports umbrella as well as some clubs outside Rutgers Esports.</li>
-                    </ul>
-                </p>
+                <div className="aboutInfo">
+                    <h2>About</h2>
+                    <h4>Rutgers Esports Discord is <em>the</em> main hub for all things Rutgers Esports!</h4>
+                    <p> {/* You can change this to whatever idrc */}
+                        <ul>
+                            <li>Get important announcements regarding events and opportunities within Rutgers Esports and the larger collegiate Esports scene in the northeast!</li>
+                            <li>Talk to the Rutgers Esports community about trending new titles, upcoming events, or even anime!</li>
+                            <li>Acts as an entry point to numerous clubs under the Rutgers Esports umbrella as well as some clubs outside Rutgers Esports.</li>
+                        </ul>
+                    </p>
+                </div>
+                <iframe className="discord-widget" title="Rutgers Esports" src="https://discord.com/widget?id=143013824679641088&theme=dark" width="350" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
             </div>
 
             <div className="servers section-container">
-                <h1 class="new-section">Server Directory</h1>
+                <h2 class="new-section">Server Directory</h2>
                 <ServersGrid gridTitle="Clubs"/>
                 <GenericServerGrid gridTitle="Other Games" items={otherGames}/>
                 {/* There's some weird spacing issue here */}
@@ -48,9 +50,9 @@ function Discord() {
                 <a href="https://github.com/sriRacha21/Rutgers-kun3/blob/master/README.md">
                     <img src="/Pictures/Branding/Rutgers-kun/Chibirkun_horizontal_text.png" alt="Rutgers-kun Horizontal"></img>
                 </a>
-                <h4 class="important">
+                <p class="important">
                     Rutgers-kun is a bot made within Rutgers Esports that manages this server and 50+ other Rutgers Servers.
-                </h4>
+                </p>
                 <div className="social-links d-flex justify-content-center">
                     <div className="horizontal-group justify-content-center">
                         <a href="https://github.com/sriRacha21/Rutgers-kun3/blob/master/README.md">
