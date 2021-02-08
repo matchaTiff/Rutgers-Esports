9f0cc6e169c87d171f9eddf5c06ba34af789e163
unknown <giannagrace008@gmail.com>
2021-02-07 22:30:35 -0500

    update path in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import logosdata from "../data/logosdata";
+import logosdata from "../data/logosdata.js";
 
 function Navbar() {
   return (
